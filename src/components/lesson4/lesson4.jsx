5f4a0b1c0c36ebc5168d2ff3549e7afab8395fd3
ManI <letz0703@gmail.com>
2022-07-27 15:46:32 +0900

    lesson5

--- src/components/lesson4/lesson4.jsx
+++ src/components/lesson4/lesson4.jsx
@@ -1,29 +1,49 @@
+import {buildQueries} from '@testing-library/react';
 import React, {useState} from 'react';
+import styles from "./lesson4.module.css";
 
 const Lesson4 = () => {
-  const [todoList, setTodoList] = useState([]);
+  const title = 'Lesson 4';
+  const subTitle = 'Todo list';
   const [newTask, setNewTask] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
 
   const handleNewTaskInput = (e) => {
     setNewTask(e.target.value);
   };
 
-  const addTask = () => setTodoList([...todoList, newTask]);
+  const addTask = () =>
+  {
+    const task = {
+      id: todoList.length === 0 ? 1: todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    }
+    setTodoList([...todoList, task]);
+  }
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter(task => task.id !== id);
+    setTodoList(newTodoList);
+  };
 
   return (
-    <div>
-      <h1>Lesson 4</h1>
-      <p>TODO list</p>
-      <div className='add-task'></div>
+    <div className={styles.lesson4}>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
       <input type='text' onChange={handleNewTaskInput} />
       <button onClick={addTask}>Click me</button>
-      <div className='list'>
+      <div className={styles.list}>
         {todoList.map((task) => {
           return (
-            <>
-              <h1>{task}</h1>
-              <button>x</button>
-            </>
+            <div key={task.id}>
+              <span >{task.taskName}</span>
+              <button
+              className={styles.button}
+              onClick={() => {
+                deleteTask(task.id);
+              }}>x</button>
+            </div>
           );
         })}
       </div>
