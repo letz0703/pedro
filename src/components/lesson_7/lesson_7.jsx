c38bdae7fcfd5d77fb5a268aece8382f8bb885fc
ManI <letz0703@gmail.com>
2022-08-03 12:50:29 +0900

    axios fetch

--- src/components/lesson_7/lesson_7.jsx
+++ src/components/lesson_7/lesson_7.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
 //import styles from './lesson_7.module.css'
 
-fetch('https://catfact.ninja/fact') //
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch('https://catfact.ninja/fact') //
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
 
 function Lesson7() {
-  return <div>Lesson7</div>;
+  const [cat, setCat] = useState('');
+
+  const fetchCat = async () => {
+    const res = await Axios.get('https://catfact.ninja/fact');
+    setCat(res.data.fact);
+  };
+
+  useEffect(() => fetchCat(), []);
+
+  // useEffect(() => {
+  //   Axios.get('https://catfact.ninja/fact') //
+  //     .then((res) => setCat(res.data.fact));
+  // }, []);
+
+  return (
+    <div>
+      <h1>Lesson7</h1>
+      <button onClick={fetchCat}>Generate Cat Fact</button>
+      <p>{cat}</p>
+    </div>
+  );
 }
 
 export default Lesson7;
