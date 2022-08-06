bd509a4fbc1afe4549d407fcac2337aa263b57a2
ManI <letz0703@gmail.com>
2022-08-07 00:04:50 +0900

    Hook : createContext value={{ , useContext(AppContext)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, createContext} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/home/home';
 import Jap from './components/jap/jap';
@@ -7,18 +8,24 @@ import Can from './components/can/can';
 import NotFound from './components/not_found/not_found';
 import Nav from './components/nav/nav';
 
+export const AppContext = createContext();
+
 function App() {
+  const [japitem, setJapitem] = useState('');
+
   return (
     <div className='App'>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/jap' element={<Jap />} />
-          <Route path='/can' element={<Can />} />
-          <Route path='*' element={<NotFound />} /> // Not Found
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{japitem, setJapitem}}>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/jap' element={<Jap />} />
+            <Route path='/can' element={<Can />} />
+            <Route path='*' element={<NotFound />} /> // Not Found
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/jap/jap.jsx
+++ src/components/jap/jap.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
 //import styles from './jap.module.css'
+import {useState, useContext} from 'react';
+import {AppContext} from '../../App';
 
 const Jap = () => {
-  return <div>Order to Japan</div>;
+  // const {japitem} = useParams();
+  const [input, setInput] = useState('');
+  const {japitem, setJapitem} = useContext(AppContext);
+
+  return (
+    <>
+      <input
+        onChange={(eve) => {
+          setInput(eve.currentTarget.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setJapitem(input);
+        }}
+      >
+        update item
+      </button>
+      <div>Order {japitem} to Japan</div>
+    </>
+  );
 };
 
 export default Jap;
