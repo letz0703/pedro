b50ef7f9e8ed9e229ae8f771a9dc2e068a47512a
ManI <letz0703@gmail.com>
2022-08-09 00:43:33 +0900

    3 line menu

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, {useState, Link} from 'react';
 import styles from './home.module.css';
 
 const Home = () => {
+  const [menu, setMenu] = useState(false);
   return (
     <div className={styles.container}>
-      <input type='checkbox' id='menuicon' />
-      <label htmlFor='menuicon' className={styles.menuicon}>
-        <span className={styles.item}></span>
-        <span className={styles.item}></span>
-        <span className={styles.item}></span>
-      </label>
-      <div className={styles.item}>일본주문</div>
-      <div className={styles.item}>통관번호 필요 / 일주일간 해외주문 금지</div>
+      <div className={styles.threeline}>
+        <input
+          type='checkbox'
+          id='menuicon'
+          onChange={(eve) => {
+            setMenu(!menu);
+          }}
+        />
+        <label htmlFor='menuicon' className={styles.menuicon}>
+          <span className={styles.item}></span>
+          <span className={styles.item}></span>
+          <span className={styles.item}></span>
+        </label>
+      </div>
+      {menu && (
+        <div className={styles.intro}>
+          <ul>
+            <li>
+              <h1>일본주문</h1>
+              {/* <span>국내 최저가/ 배송비 전액무료 / 당일 발송</span> */}
+            </li>
+            <li>
+              <h1>공동구매</h1>
+              {/* <span>오프라인보다 저렴/수량 한정 / 당일 발송</span> */}
+            </li>
+            <li>
+              <h1>부산 깡통시장 구매대행</h1>
+              {/* <span>깡통시장 오프라인 가격 + 택배비</span> */}
+            </li>
+          </ul>
+        </div>
+      )}
+      <div className={styles.japitem}>
+        <h1> Oder To Japn</h1>
+        <p>일본주문 / 선배송 후주문</p>
+        일본에 주문하는 상품입니다.
+        <br /> 먼저 상품을 보내드린 후 일본에 주문합니다.
+        <br />
+        해외 운임, 국내 배송비 모두 공짜 입니다.
+        <br />
+        저희 단골께만 소량 제공되는 특별가 수입대행 서비스 입니다.
+        <br />
+        <button>일본주문상세보기</button>
+      </div>
+
+      {/* <div className={styles.item}>일본주문</div>
+      <div className={styles.item}>통관번호 필요 / 일주일간 해외주문 금지</div> */}
     </div>
   );
 };
