b2842d32edef7a9038eaf0418d4933ff0ba5889a
ManI <letz0703@gmail.com>
2022-08-08 17:44:59 +0900

    menu

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,9 +4,12 @@ import styles from './home.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>Hello Flex</div>
-      <div className={styles.item}>abc</div>
-      <div className={styles.item}>helloflex</div>
+      <input type='checkbox' id='menuicon' />
+      <label htmlFor='menuicon' className={styles.menuicon}>
+        <span className={styles.item}></span>
+        <span className={styles.item}></span>
+        <span className={styles.item}></span>
+      </label>
     </div>
   );
 };
