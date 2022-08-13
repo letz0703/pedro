f2d7c035baf56aa2338972c2f894b83f005d04de
ManI <letz0703@gmail.com>
2022-08-13 16:37:46 +0900

    2022-08-13 SAT

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState, createContext} from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/home/home';
-import Jap from './components/jap/jap';
-import Can from './components/can/can';
-import Together from './components/together/together';
-import NotFound from './components/not_found/not_found';
+import logo from "./logo.svg";
+import "./App.css";
+import {useState, createContext} from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/home/home";
+import Jap from "./components/jap/jap";
+import Can from "./components/can/can";
+import Together from "./components/together/together";
+import NotFound from "./components/not_found/not_found";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-import Nav from './components/nav/nav';
+import Nav from "./components/nav/nav";
 
 export const AppContext = createContext();
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 function App() {
   const [japitem, setJapitem] = useState(null);
 
   return (
     <div className='App'>
-      <AppContext.Provider value={{japitem, setJapitem}}>
-        <Router>
-          <Nav />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/jap' element={<Jap />} />
-            <Route path='/together' element={<Together />} />
-            <Route path='*' element={<NotFound />} /> // Not Found
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{japitem, setJapitem}}>
+          <Router>
+            <Nav />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/jap' element={<Jap />} />
+              <Route path='/together' element={<Together />} />
+              <Route path='*' element={<NotFound />} /> // Not Found
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,33 +1,32 @@
-import React, {useState, Link} from 'react';
-import styles from './home.module.css';
-import Hambuger from '../hambuger/hambuger';
-import Intro from '../intro/intro';
+import React, {useState, Link} from "react";
+import styles from "./home.module.css";
+import Hambuger from "../hambuger/hambuger";
+import Intro from "../intro/intro";
+import {useQuery} from "@tanstack/react-query";
+import Axios from "axios";
 
 const Home = () => {
   const [menu, setMenu] = useState(false);
   const [showmenu, setShowmenu] = useState(false);
+  const {data, isLoading, isError, refetch} = useQuery(["cat"], async () => {
+    const res = await Axios.get("https://catfact.ninja/fact");
+    return res.data;
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.threeline}>
         <Hambuger showmenu={showmenu} setShowmenu={setShowmenu} />
       </div>
 
-      <div>{showmenu && <Intro />}</div>
-
-      <div className={styles.japitem}>
-        <h1> Oder To Japn</h1>
-        <p>일본주문 / 선배송 후주문</p>
-        일본에 주문하는 상품입니다.
-        <br /> 먼저 상품을 보내드린 후 일본에 주문합니다.
-        <br />
-        해외 운임, 국내 배송비 모두 공짜 입니다.
-        <br />
-        저희 단골께만 소량 제공되는 특별가 수입대행 서비스 입니다.
-        <br />
-        <button>일본주문상세보기</button>
+      <div>
+        {showmenu && (
+          <>
+            <span>{data?.fact}</span>
+            <Intro />
+          </>
+        )}
       </div>
-      {/* <div className={styles.item}>일본주문</div>
-      <div className={styles.item}>통관번호 필요 / 일주일간 해외주문 금지</div> */}
     </div>
   );
 };
