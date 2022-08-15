f15b79fbdf4f13a047ab04f6c9eb766466334cd8
ManI <letz0703@gmail.com>
2022-08-15 17:55:32 +0900

    2022-08-15 : jap.jsx

--- src/components/jap/jap.jsx
+++ src/components/jap/jap.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
-//import styles from './jap.module.css'
-import {useState, useContext} from 'react';
-import {AppContext} from '../../App';
+import React from "react";
+import {useParams} from "react-router-dom";
+import styles from "./jap.module.css";
+import {useState, useContext} from "react";
+import {AppContext} from "../../App";
+import {useForm} from "react-hook-form";
 
 const Jap = () => {
   // const {japitem} = useParams();
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const {japitem, setJapitem} = useContext(AppContext);
+
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
-      <input
-        onChange={(eve) => {
-          setInput(eve.currentTarget.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          setJapitem(input);
-        }}
-      >
-        update item
-      </button>
-      <div>Order {japitem} to Japan</div>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <h1>Order to Japan</h1>
+          <input onChange={handleSubmit(onSubmit)} />
+          <button
+            onClick={() => {
+              setJapitem(input);
+            }}
+          >
+            상품추가
+          </button>
+        </div>
+      </div>
     </>
   );
 };
